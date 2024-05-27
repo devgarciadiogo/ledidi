9b71402895f2f3aff6e668c2aa258c06bc215fec
Diogo Garcia <diogogarcia0511@gmail.com>
2024-05-26 22:15:19 -0300

    v3.2.1
    - Começando a alterar o contexto

--- index.php
+++ index.php
@@ -49,6 +49,30 @@ if ($tipo == 1) {
             $matricula = $_GET['matricula'];
 
             $resultado = $db_context->atualizar($id, $nome, $matricula)
+            echo $resultado;
+
         }
+        else{
+            $error = array('error' => 'Parametro ID, NOME ou MATRICULA nao indicado ou requisicao');
+            echo json_encode($error);
+        }
+        else if ($tipo == 4){
+            
+            if (isset($_GET['id'])) {
+                
+                $id = $_GET['id'];
+
+                $resultado = $db_context->deletar($id);
+                echo $resultado;
+            }
+            else {
+                $error = array('error' => 'parametro ID nao indicado na requisicao');
+                echo json_encode($error);
+            }
+        }
+
+        $db_context-> desconectar();
     }
 }
+
+?>
--- script.js
+++ script.js
@@ -1,97 +1,70 @@
-document.getElementById('cadastro').addEventListener('click', function(){
-    var nome = document.getElementById("nomeCadastro").value;
-    var tel = document.getElementById("telCadastro").value;
-    var email = document.getElementById("emailCadastro").value;
-    var senha = document.getElementById("senhaCadastro").value;
+$(document).ready(function() {
+    $('#cadastro').click(function(event) {
+        var nome = $('#nomeCadastro').val();
+        var tel = $('#telCadastro').val();
+        var email = $('#emailCadastro').val();
+        var senha = $('#senhaCadastro').val();
 
-    fazerRequisição(1, undefined, nome, tel, email, senha);
-    fazerRequisição(2, undefined,undefined, undefined);
-});
-
-document.getElementById('listar').addEventListener('click', function(){
-    fazerRequisição(2, undefined, undefined, undefined);
-})
-
-document.getElementById('atualizar-dados').addEventListener('click', function(){
-    var nome = document.getElementById('nome').value
-    var tel = document.getElementById('tel').value
-    var email = document.getElementById('email').value
-    var senha = document.getElementById('senha').value
-
-    fazerRequisição(3,nome,tel,email,senha);
-    fazerRequisição(2, undefined,undefined,undefined)
-});
-
-document.getElementById('deletar-dados').addEventListener('click', function(){
-    var nome = document.getElementById('nome').value
-    var tel = document.getElementById('tel').value
-    var email = document.getElementById('email').value
-    var senha = document.getElementById('senha').value
-
-    fazerRequisição(4, id, undefined, undefined, undefined, undefined)
-    fazerRequisição(2, undefined, undefined, undefined, undefined, undefined)
-})
-
-function fazerRequisição(tipo, id, nome, tel, email, senha){
-    var url = `http://localhost/ledidi/index.php?tipo=${tipo}&`
-
-    if(id != undefined){
-        url += `id=${id}&`
-    }
-    if(nome != undefined){
-        url += `id=${nome}&`
-    }
-    if(tel != undefined){
-        url += `id=${tel}&`
-    }
-    if(email != undefined){
-        url += `id=${email}&`
-    }
-    if(senha != undefined){
-        url += `id=${senha}&`
-    }
-
-    fetch(url, {method: 'get'}).then(function(response){
-        if(tipo == 2){
-            return response.json();
-        }
-    }).then(function(data){
+        fazerRequisição(1, undefined, nome, tel, email, senha);
+        fazerRequisição(2, undefined, undefined, undefined);
         
-        let alunos = data;
-        
-        let table = document.getElementsByName('table');
-
-        let linhas = "";
-
-        for (var i = 0; i < alunos.length; i++) {
-        linhas += "<tr>"
-        +    `<td> ${alunos[i].id}</td>`
-        +    `<td> ${alunos[i].nome}</td>`
-        +    `<td> ${alunos[i].tel}</td>`
-        +    `<td> ${alunos[i].email}</td>`
-        +     `<td> ${alunos[i].senha}</td>`
-        +  "</tr>";
-        }
-        
-
-        table[0].innerHTML = ""
-
-        table[0].innerHTML = "<tr>"
-        +"<th>Id</th>"
-        +"<th>nome</th>"
-        +"<th>tel</th>"
-        +"<th>email</th>"
-        +"<th>senha</th>"
-        +linhas;
-    })
-
-    .catch(function(error){
-        console.log(error);
+        event.preventDefault(); // Impede o envio do formulário
     });
 
-    document.getElementById('id').value = "";
-    document.getElementById('nome').value = "";
-    document.getElementById('tel').value = "";
-    document.getElementById('email').value = "";
-    document.getElementById('senha').value = "";
-}
+    $('#listar').click(function() {
+        fazerRequisição(2, undefined, undefined, undefined);
+    });
+
+    $('#atualizar-dados').click(function() {
+        var nome = $('#nome').val();
+        var tel = $('#tel').val();
+        var email = $('#email').val();
+        var senha = $('#senha').val();
+
+        fazerRequisição(3, undefined, nome, tel, email, senha);
+        fazerRequisição(2, undefined, undefined, undefined);
+    });
+
+    $('#deletar-dados').click(function() {
+        var nome = $('#nome').val();
+        var tel = $('#tel').val();
+        var email = $('#email').val();
+        var senha = $('#senha').val();
+
+        fazerRequisição(4, undefined, nome, tel, email, senha);
+        fazerRequisição(2, undefined, undefined, undefined);
+    });
+
+    function fazerRequisição(tipo, id, nome, tel, email, senha) {
+        var url = `http://localhost/ledidi/index.php?tipo=${tipo}&`;
+
+        if (id != undefined) {
+            url += `id=${id}&`;
+        }
+        if (nome != undefined) {
+            url += `nome=${nome}&`;
+        }
+        if (tel != undefined) {
+            url += `tel=${tel}&`;
+        }
+        if (email != undefined) {
+            url += `email=${email}&`;
+        }
+        if (senha != undefined) {
+            url += `senha=${senha}&`;
+        }
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                // Aqui você pode manipular a resposta, se necessário
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.error('Erro na requisição:', xhr, status, error);
+            }
+        });
+    }
+});
