98b9d82370309dccff50acac1b6b2f06eb26d43b
LeonardoIBraim <andrearia2015@gmail.com>
2024-05-30 11:18:58 -0300

    v3.2.4
    -ajustamos o nosso código (a parte do javascript)

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-document.getElementById('cadastrar').addEventListener('click', function() {
-    var nome = document .getElementById('*nome').value;
-    var matricula = document .getElementById('matricula').value;
-    fazerRequisicao(1, undefined, nome, matricula);
-    fazerRequisicao(2, undefined, undefined, undefined);
+document.getElementById('cadastro').addEventListener('click', function() {
+    var nome = document.getElementById('#nomeCadastro').value;
+    var telefone = document.getElementById('#telCadastro').value;
+    var email = document.getElementById('#emailCadastro').value;
+    var senha = document.getElementById('#senhaCadastro').value;
+    fazerRequisicao(1, undefined, nome, telefone, email, senha);
+        window.location.href = 'dados.html';
+    fazerRequisicao(2, undefined, undefined, undefined, undefined, undefined);
 });
     
 document.getElementById('listar').addEventListener('click', function() {
-    fazerRequisicao(2, undefined, undefined, undefined);
+    fazerRequisicao(2, undefined, undefined, undefined, undefined, undefined);
 });
 
 document.getElementById('atualizar').addEventListener('click', function (){
     var id = document.getElementById('id').value;
-    var nome = document.getElementById('nome').value;
-    var matricula = document.getElementById('matricula').value
+    var nome = document.getElementById('#nomeCadastro').value;
+    var telefone = document.getElementById('#telCadastro').value
+    var email = document.getElementById('#emailadastro').value
+    var senha = document.getElementById('#senhaCadastro').value
 
-    fazerRequisicao(3, id, nome, matricula);
-    fazerRequisicao(2, undefined, undefined, undefined)
+    fazerRequisicao(3, id, nome, telefone, email, senha);
+    fazerRequisicao(2, undefined, undefined, undefined, undefined, undefined)
 });
 
 document.getElementById('deletar').addEventListener('click', function(){
     var id = document.getElementById('id').value
 
-    fazerRequisicao(4, id, undefined, undefined)
-    fazerRequisicao(2, undefined, undefined,undefined)
+    fazerRequisicao(4, id, undefined, undefined, undefined, undefined)
+    fazerRequisicao(2, undefined, undefined,undefined, undefined, undefined)
 });
 
-function fazerRequisicao(tipo, id, nome, matricula){
-    var url = `http://localhost/ProjetoAlunos/back/index.php=${tipo}&`
+function fazerRequisicao(tipo, id, nome, telefone, email, senha){
+    var url = `http://localhost/ledidi/index.php=${tipo}&`
 
     if (id != undefined){
         url += `id=${id}&`;
@@ -36,8 +41,14 @@ function fazerRequisicao(tipo, id, nome, matricula){
         url += `nome=${nome}&`;
     }
 
-    if (matricula != undefined){
-        url += `matricula=${matricula}&`
+    if (telefone != undefined){
+        url += `tel=${telefone}&`
+    }
+    if (email != undefined){
+        url += `email=${email}&`
+    }
+    if (senha != undefined){
+        url += `senha=${senha}&`
     }
 
     fetch(url, { method: 'get'}).then(function(response){
@@ -54,9 +65,10 @@ function fazerRequisicao(tipo, id, nome, matricula){
         for (var i = 0; i < alunos.lenght; i++) {
         linhas += "<tr>"
         +   `<td>${alunos[i].id}</td>`
-        +   `<td>${alunos[i].id}</td>`
-        +   `<td>${alunos[i].id}</td>`
-        +   `<td>${alunos[i].id}</td>`
+        +   `<td>${alunos[i].nome}</td>`
+        +   `<td>${alunos[i].telefone}</td>`
+        +   `<td>${alunos[i].email}</td>`
+        +   `<td>${alunos[i].senha}</td>`
         + "</tr>"
         }
 
@@ -65,7 +77,9 @@ function fazerRequisicao(tipo, id, nome, matricula){
         table[0].innerHTML = "<tr>"
         +"<th>id</th>"
         +"<th>Nome</th>"
-        +"<th>Matricula</th>"
+        +"<th>Telefone</th>"
+        +"<th>Email</th>"
+        +"<th>Senha</th>"
         +"</tr>"
         + linhas;
         })
@@ -76,8 +90,10 @@ function fazerRequisicao(tipo, id, nome, matricula){
     });
 
     document.getElementById('id').value = "";
-    document.getElementById('nome').value = "";
-    document.getElementsById('matricula').value = "";
+    document.getElementById('#nomeCadastro').value = "";
+    document.getElementsById('#telCadastro').value = "";
+    document.getElementsById('#emailCadastro').value = "";
+    document.getElementsById('#senhaCadastro').value = "";
    
 }
 
@@ -111,7 +127,7 @@ function fazerRequisicao(tipo, id, nome, matricula){
 
 
 
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('#formCadastro').submit(function(event) {
         event.preventDefault(); // Impede o envio do formulário
 
@@ -150,4 +166,4 @@ $(document).ready(function() {
             }
         });
     });
-});
+});*/
